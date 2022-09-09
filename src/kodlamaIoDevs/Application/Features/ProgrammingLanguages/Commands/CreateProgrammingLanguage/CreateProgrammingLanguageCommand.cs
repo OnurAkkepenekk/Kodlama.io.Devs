2ffc5c6dce8ba3319d4d209f3b8f9e32f86c9124
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-09-09 19:54:28 +0300

    initial commit

---
+++ src/kodlamaIoDevs/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommand : IRequest<CreatedProgrammingLanguageDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateProgrammingLanguageCommandHandler : IRequestHandler<CreateProgrammingLanguageCommand, CreatedProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+
+            public CreateProgrammingLanguageCommandHandler(IMapper mapper, IProgrammingLanguageRepository programmingLanguageRepository)
+            {
+                _mapper = mapper;
+                _programmingLanguageRepository = programmingLanguageRepository;
+            }
+
+            public async Task<CreatedProgrammingLanguageDto> Handle(CreateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                ProgrammingLanguage _mappedProgrammingLanguage = _mapper.Map<ProgrammingLanguage>(request);
+                ProgrammingLanguage _createdProgrammingLanguage = await _programmingLanguageRepository.AddAsync(_mappedProgrammingLanguage);
+                CreatedProgrammingLanguageDto createdProgrammingLanguageDto =
+                    _mapper.Map<CreatedProgrammingLanguageDto>(_createdProgrammingLanguage);
+                return createdProgrammingLanguageDto;
+
+            }
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Models;
+using Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage;
+using Core.Application.Requests;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingLanguageQuery query = new GetListProgrammingLanguageQuery { PageRequest = pageRequest };
+            ProgrammingLanguageListModel result = await Mediator.Send(query);
+            return Created("", result);
+        }
+    }
+}
